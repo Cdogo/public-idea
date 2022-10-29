697ae778ed157609476f3d79269f7d4385ecae6b
Cdogo <john.tanaristy@gmail.com>
2022-10-29 12:58:22 -0500

    sneaky feature more

--- pages/api/img.js
+++ pages/api/img.js
@@ -1,6 +1,6 @@
 export default function Img(req, res){
     var date = new Date();
-    console.log(date.toUTCString())
+    console.log(`${req.query}: ${date.toUTCString()}`)
     var u = new URL('/', 'http://localhost:3000')
     res.redirect(307, '/fear.png')
 }
