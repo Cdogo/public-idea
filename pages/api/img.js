e8c9e8dd81d3a9e899d5bafbe854e21e1673cec6
Cdogo <john.tanaristy@gmail.com>
2022-10-29 19:32:58 -0500

    Made the sneaky feature better

--- pages/api/img.js
+++ pages/api/img.js
@@ -1,6 +1,14 @@
 export default function Img(req, res){
+    let ip;
     var date = new Date();
-    console.log(`${req.query}: ${date.toUTCString()}`)
-    var u = new URL('/', 'http://localhost:3000')
+    console.log(`${req.query.id ? req.query.id: 'No ID'}: ${date.toUTCString()}`)
+    if (req.headers["x-forwarded-for"]) {
+        ip = req.headers["x-forwarded-for"]
+      } else if (req.headers["x-real-ip"]) {
+        ip = req.connection.remoteAddress
+      } else {
+        ip = req.connection.remoteAddress
+      }
+    console.log(req.connection.remoteAddress)
     res.redirect(307, '/fear.png')
 }
