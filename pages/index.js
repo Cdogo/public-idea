b2e68e996df880da6702a589b1a4709aa4b09075
Cdogo <40430704+Cdogo@users.noreply.github.com>
2023-04-04 22:36:35 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Feed from '../components/feed'
+//import Feed from '../components/feed'
 
 import styles from '../styles/Home.module.css'
-
+/*
 export default function Home() {
   return (
     <div className={styles.container}>
       <Feed/>
+    </div>
+  )
+}
+*/
+export default function Home() {
+  return (
+    <div className={styles.container}>
     </div>
   )
 }
