8b636e241737e60ffbcc0632f2a97abad7fa079d
Cdogo <40430704+Cdogo@users.noreply.github.com>
2023-04-04 22:34:30 -0500

    Update img.js

--- pages/api/img.js
+++ pages/api/img.js
@@ -1,6 +1,6 @@
 export default function Img(req, res){
-    let ip;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
     var date = new Date();
-    console.log(`${req.query.id ? req.query.id: 'No ID'}: ${date.toUTCString()}`)
+    console.log(`${req.query.id ? req.query.id: 'No ID'}: ${date.toUTCString()} ip: ${ip}`)
     res.redirect(307, '/fear.png')
 }
